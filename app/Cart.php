aed00cd0c75de91dfa2b15a9054f3a10eab43e4f
milapappy <milapappy@github.com>
2019-10-06 23:56:06 +0300

    ShoppingCart Implementation
    Signed-off-by: milapappy <milapappy@github.com>

--- app/Cart.php
+++ app/Cart.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    protected $fillable = ['user_id', 'product_id'];
 }
--- app/Product.php
+++ app/Product.php
@@ -13,4 +13,9 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'carts');
+    }
 }
